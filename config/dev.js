1d6d922aab989df46b784e387458e8fccc7db058
shashanksaurav21 <shashanksaurav21@gmail.com>
2023-12-12 18:39:54 +0530

    base
    base app

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://shashanksaurav21:shaPassword@cluster0.x2u0lsm.mongodb.net/",
+  cookieKey: "123123123",
+};
